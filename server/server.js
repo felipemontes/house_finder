9122a6c4a08d95d3c399650bbe300f96ec24d5c5
Felipe <felipe_londono1@hotmail.com>
2020-08-18 23:54:54 -0500

    [Add]: receiving request parameters

--- server/server.js
+++ server/server.js
@@ -3,12 +3,21 @@ const router = express.Router();
 const app = express();
 const cors = require("cors");
 const PORT = 3001;
+const selectOptions = require("./scraper");
 
 app.use(cors());
+app.use(express.json());
+
 app.use(router);
 
-router.post("/", (req, res) => {
-  res.send("Hola server").status(200);
+router.post("/", async (req, res) => {
+  const city = req.body.city;
+  const option = req.body.option;
+  const proper = req.body.property;
+  const quantity = req.body.quantity;
+  const ans = await selectOptions(city, quantity);
+  console.log(ans);
+  res.send(ans);
 });
 
 app.listen(PORT, () => {
