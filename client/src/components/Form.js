db2054b1bd2633e91b7bf96bb01bc798e9269530
Felipe <felipe_londono1@hotmail.com>
2020-08-19 20:00:19 -0500

    [Add]: created download button and error management

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
-
+import fileDownload from "js-file-download";
+import swal from "sweetalert";
+import "./styles/Form.css";
 export default class Form extends Component {
   state = {
     city: "",
@@ -23,70 +25,133 @@ export default class Form extends Component {
       property: this.state.property,
       quantity: this.state.quantity,
     };
-    const response = await axios.post("http://localhost:3001/", inf);
-    console.log(response.status);
+    if (Object.values(inf).length !== 4) {
+      this.errorManager();
+    }
+    try {
+      const response = await axios.post("http://localhost:3001/", inf);
+      if (response.status === 200) {
+        this.showDownload();
+      }
+      console.log(response.status);
+    } catch (e) {
+      this.errorManager();
+    }
+  };
+
+  showDownload = () => {
+    var content = document.getElementById("down");
+    content.style.display = "";
+  };
+
+  download = async () => {
+    const response = await axios({
+      method: "GET",
+      url: "http://localhost:3001/download",
+    });
+    window.open(fileDownload(response.data, "out.csv"));
+  };
+
+  errorManager = () => {
+    swal("Error", "Revise los datos e intente de nuevo", "error");
   };
 
   render() {
     return (
-      <div>
-        <h3>Ingresa tus datos de busqueda</h3>
-        <form onSubmit={this.onSubmit}>
-          <label>
-            Selecciona una ciudad:
-            <select
-              name="city"
-              value={this.state.city}
-              onChange={this.onChange}
-            >
-              <option value=""></option>
-              <option value="bogota">Bogota</option>
-              <option value="medellin">Medellin</option>
-            </select>
-          </label>
+      <div className="container">
+        <div className="form-container sign-up-container">
+          <h1>Encuentre su inmueble deseado</h1>
+
+          <h3>Ingresa tus datos de busqueda</h3>
+          <form onSubmit={this.onSubmit}>
+            <label>
+              Selecciona una ciudad:
+              <br />
+              <br />
+              <select
+                name="city"
+                value={this.state.city}
+                onChange={this.onChange}
+              >
+                <option value=""></option>
+                <option value="bogota">Bogota</option>
+                <option value="medellin">Medellin</option>
+              </select>
+            </label>
+            <br />
+            <br />
+            <label>
+              Selecciona tipo de inmueble:
+              <br />
+              <br />
+              <select
+                name="property"
+                value={this.state.property}
+                onChange={this.onChange}
+              >
+                <option value=""></option>
+                <option value="apto">Apartamento</option>
+                <option value="casa">Casa</option>
+              </select>
+            </label>
+            <br />
+            <br />
+            <label>
+              Selecciona una opcion:
+              <br />
+              <br />
+              <select
+                name="option"
+                value={this.state.option}
+                onChange={this.onChange}
+              >
+                <option value=""></option>
+                <option value="venta">Venta</option>
+                <option value="arriendo">Arriendo</option>
+              </select>
+            </label>
+            <br />
+            <br />
+            <label>
+              Ingresa cantidad de paginas:
+              <br />
+              <br />
+              <input
+                type="text"
+                name="quantity"
+                value={this.state.quantity}
+                onChange={this.onChange}
+              />
+            </label>
+            <br />
+            <br />
+            <button type="submit">Buscar!</button>
+          </form>
           <br />
-          <br />
-          <label>
-            Selecciona una opcion:
-            <select
-              name="property"
-              value={this.state.property}
-              onChange={this.onChange}
-            >
-              <option value=""></option>
-              <option value="apto">Apartamento</option>
-              <option value="casa">Casa</option>
-            </select>
-          </label>
-          <br />
-          <br />
-          <label>
-            Selecciona una opcion:
-            <select
-              name="option"
-              value={this.state.option}
-              onChange={this.onChange}
-            >
-              <option value=""></option>
-              <option value="venta">Venta</option>
-              <option value="arriendo">Arriendo</option>
-            </select>
-          </label>
-          <br />
-          <br />
-          <label>
-            Ingresa cantidad de paginas:
-            <input
-              type="text"
-              name="quantity"
-              value={this.state.quantity}
-              onChange={this.onChange}
-            />
-          </label>
-          <br />
-          <br />
-          <button type="submit">Buscar!</button>
-        </form>
+          <button id="down" style={{ display: "none" }} onClick={this.download}>
+            Download
+          </button>
+        </div>
+        <div className="form-container sign-in-container del">
+          <h2>House scraper</h2>
+          <p>
+            House scraper es un web scraper que obtiene su informacion de
+            fincaraiz.com.co, descarge un archivo csv con su busqueda deseada y
+            navegue entre los resultados de forma sencilla
+          </p>
+          <p>
+            La intencion de house scraper es facilitar la busqueda de
+            propiedades
+          </p>
+          <h2>Instrucciones</h2>
+
+          <li>1)Ingrese los datos de busqueda</li>
+          <li>2)Espere a que el scraper complete la busqueda</li>
+          <li>
+            3)Cuando se complete la busqueda aparecera el boton de descarga
+          </li>
+          <li>4)Descargue el archivo csv con la informacion</li>
+        </div>
       </div>
     );
   }
