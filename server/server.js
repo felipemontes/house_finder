19bb8bd0ac093cd842613b4783a175fcc8f9b0c6
Felipe <felipe_londono1@hotmail.com>
2020-08-19 20:01:55 -0500

    [Add]: created download route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const app = express();
+const fs = require("fs-extra");
 const cors = require("cors");
 const PORT = 3001;
 const selectOptions = require("./selector");
@@ -15,9 +16,24 @@ router.post("/", async (req, res) => {
   const option = req.body.option;
   const type = req.body.property;
   const quantity = req.body.quantity;
+  console.time("selectOptions");
   const ans = await selectOptions(city, quantity, option, type);
+  console.timeEnd("selectOptions");
   console.log(ans);
-  res.sendStatus(ans);
+  res.sendStatus(200);
+});
+
+router.get("/download", async (req, res) => {
+  let file = __dirname + "/out.csv";
+  res.download(file, "out.csv");
+  try {
+    setTimeout(async () => {
+      await fs.remove(file);
+      console.log("File deleted");
+    }, 2000);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 app.listen(PORT, () => {
