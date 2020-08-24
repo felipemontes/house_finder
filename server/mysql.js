fbfbcfe55e84fd4d34ef267b0145bbce75e1cdad
Felipe <felipe_londono1@hotmail.com>
2020-08-24 01:04:38 -0500

    [Add]: created connection with database

---
+++ server/mysql.js
@@ -0,0 +1,58 @@
+const mysql = require("mysql");
+const config = require("./config");
+const { json } = require("express");
+
+const dbconf = {
+  host: config.mysql.host,
+  user: config.mysql.user,
+  password: config.mysql.password,
+  database: config.mysql.database,
+};
+
+let connection;
+
+function handleCon() {
+  connection = mysql.createConnection(dbconf);
+  connection.connect((err) => {
+    if (err) {
+      console.error("[db]" + err);
+      setTimeout(handleCon, 2000);
+    } else {
+      console.log("DB connected");
+    }
+  });
+  connection.on("error", (err) => {
+    console.error("[db err]", err);
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      handleCon();
+    } else {
+      throw err;
+    }
+  });
+}
+handleCon();
+
+function download(table) {
+  return new Promise((resolve, reject) => {
+    connection.query(`SELECT * FROM ${table}`, (err, data) => {
+      if (err) return reject(err);
+      const jsonData = JSON.parse(JSON.stringify(data));
+      console.log(jsonData);
+      resolve(jsonData);
+    });
+  });
+}
+
+function insert(table, data) {
+  return new Promise((resolve, reject) => {
+    connection.query(`INSERT INTO ${table} SET ? `, data, (err, result) => {
+      if (err) return reject(err);
+      resolve(result);
+    });
+  });
+}
+
+module.exports = {
+  download,
+  insert,
+};
