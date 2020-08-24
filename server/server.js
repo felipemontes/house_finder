afc8e97ad8f7cf433c490f054ee793eae6a33e01
Felipe <felipe_londono1@hotmail.com>
2020-08-24 01:03:36 -0500

    [Add]: modified endpoints and added DB connection

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const app = express();
-const fs = require("fs-extra");
 const cors = require("cors");
-const fork = require("child_process").fork;
+const fastcsv = require("fast-csv");
+const manager = require("./selector");
+const db = require("./mysql");
+const fs = require("fs");
 const PORT = 3001;
 
 app.use(cors());
@@ -13,33 +15,27 @@ app.use(router);
 
 router.post("/", async (req, res) => {
   const city = req.body.city;
-  const option = req.body.option;
   const type = req.body.property;
-  const quantity = req.body.quantity;
-  const select = fork("selector.js");
-  select.send({
-    city,
-    option,
-    type,
-    quantity,
-  });
-  select.on("message", (ans) => {
-    res.status(200).send(ans);
-  });
+  const option = req.body.option;
+  const tb_name = city + "_" + type + "_" + option;
+  res.status(200).send(tb_name);
 });
 
-router.get("/download/:id", async (req, res) => {
-  const dId = req.params.id;
-  let file = __dirname + `/${dId}`;
-  res.download(file);
-  try {
-    setTimeout(async () => {
-      await fs.remove(file);
-      console.log("File deleted");
-    }, 2000);
-  } catch (error) {
-    console.error(error);
-  }
+router.get("/download/:tb", async (req, res) => {
+  const table = req.params.tb;
+  const jsonDb = await db.download(table);
+  const ws = fs.createWriteStream("bog_apt_arr.csv");
+  let path = __dirname + `/${table}.csv`;
+
+  fastcsv
+    .write(jsonDb, { headers: true })
+    .on("finish", () => {
+      console.log("File created successfully");
+    })
+    .pipe(ws);
+  setTimeout(() => {
+    res.download(path);
+  }, 2000);
 });
 
 app.listen(PORT, () => {
